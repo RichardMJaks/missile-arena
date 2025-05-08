072718ca64f2afd524c3a1604e7a4abdee7f88f8
RichardMJaks <richardmiikael.jaks@gmail.com>
2025-05-08 15:50:53 +0300

    Added missile knockback

--- missile/explosion.gd
+++ missile/explosion.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var force: float = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +13,13 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body:Node2D) -> void:
-	body.die()
+	if not body.dead:
+		body.die()
+
+func _on_knockback_area_body_entered(body:Node2D) -> void:
+	var vector_to_body = body.global_position - global_position
+	var dir = (vector_to_body).normalized()
+	var normalized_distance = vector_to_body.length_squared() / (12 * 12)
+	print(normalized_distance)
+	var force_mult = clampf(1 - normalized_distance, 0.5, 1)
+	body.velocity = dir * force_mult * force
