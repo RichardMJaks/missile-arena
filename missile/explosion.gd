d054519dee5611eaa4047461bab0b744b954a64b
RichardMJaks <richardmiikael.jaks@gmail.com>
2025-05-12 03:32:57 +0300

    more improvements

--- missile/explosion.gd
+++ missile/explosion.gd
@@ -2,15 +2,16 @@ extends Area2D
 
 @export var force: float = 50
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.explosion.emit()
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
 
 
 func _on_body_entered(body:Node2D) -> void:
